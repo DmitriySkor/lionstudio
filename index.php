3360f6e63489393a8c5b31a7399d4c2b8ff7ec15
ValentinGor <v.gorbatenko@mail.ru>
2019-03-28 19:00:11 +0200

    Верстка первой секции до блока с ценой

--- index.php
+++ index.php
@@ -35,28 +35,79 @@
 <body>
 
 <div class="first_section">
+    <div class="bg_first_section_left"></div>
+    <div class="top-horizontal-line"></div>
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-            <div class="header">
-                <a href="#" class="top_menu">Обучение</a>
-                <a href="#" class="top_menu"><i class="fa fa-circle"></i> Цены</a>
-                <a href="#" class="top_menu"><i class="fa fa-circle"></i> Учителя</a>
-
+                <div class="header">
+                    <a href="#" class="top_menu">Обучение</a>
+                    <a href="#" class="top_menu"><i class="fa fa-circle"></i> Цены</a>
+                    <a href="#" class="top_menu"><i class="fa fa-circle"></i> Учителя</a>
                     <div class="header_contacts">
-
-                            <a class="phone_number" href="tel:88009454555">8 800 945 45 55</a>
-                            <a class="pirple_btn" href="#">Оставить заявку</a>
-                        <img class="contact_arrow" src="/img/arrow_down.png" alt="">
+                        <a class="phone_number" href="tel:88009454555">8 800 945 45 55</a>
+                        <a class="pirple_btn" href="#">Оставить заявку</a>
+                        <img class="contact_arrow" src="/img/arrow_down.png" alt="img">
                     </div>
-
-                <a href="#" class="top_menu">Отзывы</a>
-                <a href="#" class="top_menu"><i class="fa fa-circle"></i> Работы</a>
-                <a href="#" class="top_menu"><i class="fa fa-circle"></i> Почему мы</a>
+                    <a href="#" class="top_menu">Отзывы</a>
+                    <a href="#" class="top_menu"><i class="fa fa-circle"></i> Работы</a>
+                    <a href="#" class="top_menu"><i class="fa fa-circle"></i> Почему мы</a>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="first__info">
+                    <div class="first__info_slogan">
+                        Школа мастеров перманентного макияжа в Москве
+                    </div>
+                    <div class="first__info_images">
+                        <img class="img-fluid" src="img/name.png" alt="img">
+                    </div>
+                    <div class="first__info_text">
+                        Наш преподавательский коллектив уже много лет занимается подготовкой высококлассных мастеров перманентного макияжа. Курсы подходят всем, кто
+                        интересуется этим видом искусства. Но в первую очередь они рассчитаны на тех, кто хочет не просто научиться делать безукоризненный татуаж, но также
+                        нацелен на достижение финансовой свободы и быстрое продвижение вверх по карьерной лестнице.
+                    </div>
+                </div>
+                <div class="contain-mouse">
+                    <div class="mouse">
+                        <div class="mouse__wheel"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="second__info">
+                    <div class="second__info_head">
+                        Почему профессия так популярна?
+                    </div>
+                    <div class="second__info_text">
+                        и почему для обучения выбирают именно нас?
+                    </div>
+                    <div class="second__info_number">
+                        <div class="contain-number">
+                            <div class="number">
+                                1
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="leaves">
+                    <div class="leaves__info">
+                        <div class="leaves__info_number">
+                            <i class="fa fa-circle" aria-hidden="true"></i>
+                            120 000 &#8381;
+                            <i class="fa fa-circle" aria-hidden="true"></i>
+                        </div>
+                        <div class="leaves__info_text">
+                            средний заработок мастера в Москве после окончания нашей школы
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        </div>
     </div>
+    <div class="bg_first_section_right"></div>
 </div>
 
 
